0180fed160c0f767437fe8a4b6c0c25dc468ced8
Rafael Nicdao <nicdaoraf@gmail.com>
2021-02-25 23:21:27 +1100

    Fix dired script

--- scripts/dired.sh
+++ scripts/dired.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-emacsclient -c -t
+emacsclient -c \
   --eval "(dired default-directory)" \
   --eval "(defun kill-window-or-emacs () (interactive) (if (one-window-p) (kill-emacs) (delete-window)))" \
   --eval "(setq dired-dwim-target t delete-by-moving-to-trash t)" \
