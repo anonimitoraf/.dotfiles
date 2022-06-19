fb4778064b0aa9284a325cb40794dc137bbebd62
Rafael Nicdao <nicdaoraf@gmail.com>
2022-06-19 22:19:01 +1000

    Add hammerspoon config

---
+++ .hammerspoon/init.lua
@@ -0,0 +1,73 @@
+local spaces = require("hs.spaces")
+
+-- Switch alacritty
+hs.hotkey.bind({'command'}, 'return', function ()
+  local APP_NAME = 'Alacritty'
+  function moveWindow(alacritty, space, mainScreen)
+    -- move to main space
+    local win = nil
+    while win == nil do
+      win = alacritty:mainWindow()
+    end
+    print(win)
+    print(space)
+    print(win:screen())
+    print(mainScreen)
+    local fullScreen = not win:isStandard()
+    if fullScreen then
+      hs.eventtap.keyStroke('cmd', 'return', 0, alacritty)
+    end
+    winFrame = win:frame()
+    scrFrame = mainScreen:fullFrame()
+    print(winFrame)
+    print(scrFrame)
+    winFrame.w = scrFrame.w
+    winFrame.y = scrFrame.y
+    winFrame.x = scrFrame.x
+    print(winFrame)
+    win:setFrame(winFrame, 0)
+    print(win:frame())
+    -- win:spacesMoveTo(space)
+    spaces.moveWindowToSpace(win, space)
+    if fullScreen then
+      hs.eventtap.keyStroke('cmd', 'return', 0, alacritty)
+    end
+    win:focus()
+  end
+  local alacritty = hs.application.get(APP_NAME)
+  if alacritty ~= nil and alacritty:isFrontmost() then
+    alacritty:hide()
+  else
+    -- local space = spaces.activeSpace()
+    local space = spaces.focusedSpace()
+    -- local mainScreen = hs.screen.find(spaces.mainScreenUUID())
+    local mainScreen = hs.screen.mainScreen()
+    if alacritty == nil and hs.application.launchOrFocus(APP_NAME) then
+      local appWatcher = nil
+      print('create app watcher')
+      appWatcher = hs.application.watcher.new(function(name, event, app)
+        print(name)
+        print(event)
+        if event == hs.application.watcher.launched and name == APP_NAME then
+          app:hide()
+          moveWindow(app, space, mainScreen)
+          appWatcher:stop()
+        end
+      end)
+      print('start watcher')
+      appWatcher:start()
+    end
+    if alacritty ~= nil then
+      moveWindow(alacritty, space, mainScreen)
+    end
+  end
+end)
+
+-- Hide alacritty if not in focus
+hs.window.filter.default:subscribe(hs.window.filter.windowFocused, function(window, appName)
+  local alacritty = hs.application.get('Alacritty')
+  if alacritty ~= nil then
+     alacritty:hide()
+  end
+end)
+
