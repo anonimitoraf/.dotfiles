d345ed782ead5053e4c7b3cafa95f9a44f859790
Rafael Nicdao <nicdaoraf@gmail.com>
2022-07-10 00:25:20 +1000

    Move window to other screen with 'cmd l'

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -71,3 +71,13 @@ hs.window.filter.default:subscribe(hs.window.filter.windowFocused, function(wind
   end
 end)
 
+-- bind hotkey
+hs.hotkey.bind({'cmd'}, 'l', function()
+  -- get the focused window
+  local win = hs.window.focusedWindow()
+  -- get the screen where the focused window is displayed, a.k.a. current screen
+  local screen = win:screen()
+  -- compute the unitRect of the focused window relative to the current screen
+  -- and move the window to the next screen setting the same unitRect
+  win:move(win:frame():toUnitRect(screen:frame()), screen:next(), true, 0)
+end)
