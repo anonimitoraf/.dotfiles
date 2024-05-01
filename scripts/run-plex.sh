04cff4b5a4f6ab415367f5452be76a3abfea4c19
anonimitoraf <nicdaoraf@gmail.com>
2024-05-02 08:36:22 +1000

    Add script for starting plex server

---
+++ scripts/run-plex.sh
@@ -0,0 +1,12 @@
+docker run -d \
+  --name=plex \
+  --net=host \
+  -e PUID=1000 \
+  -e PGID=1000 \
+  -e TZ=Australia/Melbourne \
+  -e VERSION=docker \
+  -v /mnt/8tb/library:/config \
+  -v /mnt/8tb/tv-series:/tv \
+  -v /mnt/8tb/movies:/movies \
+  --restart unless-stopped \
+  lscr.io/linuxserver/plex:latest
